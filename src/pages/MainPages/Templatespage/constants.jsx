0127eeb248fe614ad671787c8191cacd8dacd205
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-03-19 15:46:12 +0700

    Update full source code

--- src/pages/MainPages/Templatespage/constants.jsx
+++ src/pages/MainPages/Templatespage/constants.jsx
@@ -1,7 +1,15 @@
+/**
+ * Imports various UI component templates from their respective modules.
+ */
 import { headers } from '../../../../templates/headers';
 import { footers } from '../../../../templates/footers';
 import { buttons } from '../../../../templates/buttons';
 import { cards } from '../../../../templates/cards';
 import { alerts } from '../../../../templates/alerts';
 
+/**
+ * Combines all imported UI component templates into a single array.
+ * 
+ * @constant {Array} templates - An array containing all header, footer, button, card, and alert templates.
+ */
 export const templates = [...headers, ...footers, ...buttons, ...cards, ...alerts];
